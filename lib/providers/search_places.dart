605c6bbf90734875eb723ee7d8e21fb5a10293ae
aditya-ranjan <ranjanaditya263@gmail.com>
2024-02-24 23:51:55 +0530

    Google Map

--- lib/providers/search_places.dart
+++ lib/providers/search_places.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:googlemapsapp/models/auto_complete_result.dart';
 
 final placeResultsProvider = ChangeNotifierProvider<PlaceResults>((ref) {
   return PlaceResults();
 });
 
-class PlaceResults extends ChangeNotifier{
+final searchToggleProvider = ChangeNotifierProvider<SearchToggle>((ref) {
+  return SearchToggle();
+});
 
+class PlaceResults extends ChangeNotifier {
+  List<AutoCompleteResult> allReturnedResults = [];
+
+  void setResults(allPlaces) {
+    allReturnedResults = allPlaces;
+    notifyListeners();
+  }
+}
+
+class SearchToggle extends ChangeNotifier {
+  bool searchToggle = false;
+
+  void toggleSearch() {
+    searchToggle = !searchToggle;
+    notifyListeners();
+  }
 }
